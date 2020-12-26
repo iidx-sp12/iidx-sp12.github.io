dbe78bc01c216fbf1b4bea9752f72583aa576b3c
iidx-sp12 <71324029+iidx-sp12@users.noreply.github.com>
2020-12-27 00:27:29 +0900

    Add files via upload

---
+++ 12table.js
@@ -0,0 +1,20 @@
+$(function() {
+  $("#tbl").tablesorter();
+  
+  $.getJSON("songs.json", function(data) {
+    var tb = $("#tbl tbody");
+    var len = data.length;
+    for(let i = 0; i < len; i++) {
+      let row = "";
+      row += "<tr>";
+      row += "<td>" + data[i].name + "</td>";
+      row += "<td class=\"\{sortValue:" + data[i].d_value + "\}\">" + data[i].difficulty + "</td>";
+      row += "<td class=\"\{sortValue:" + data[i].n_value + "\}\">" + data[i].normal + "</td>";
+      row += "<td class=\"\{sortValue:" + data[i].h_value + "\}\">" + data[i].hard + "</td>";
+      row += "<td>" + data[i].version + "</td>";
+      row += "</tr>";
+      tb.append(row);
+    }
+    $("#tbl").trigger("update");
+  });
+});
