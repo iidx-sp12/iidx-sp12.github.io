4404e3e48ec78ee54769854d3bec07ea0ba57a9b
iidx-sp12 <71324029+iidx-sp12@users.noreply.github.com>
2021-08-28 19:13:42 +0900

    Add files via upload
    TOPへ戻るボタン追加

--- 12table.js
+++ 12table.js
@@ -22,4 +22,20 @@ $(function() {
     $("#tbl").trigger("update");
     $("#number").html("<b>" + len + "譜面</b>");
   });
+
+  var pagetop = $('#page_top');
+  // ボタン非表示
+  pagetop.hide();
+  // 100px スクロールしたらボタン表示
+  $(window).scroll(function () {
+     if ($(this).scrollTop() > 100) {
+          pagetop.fadeIn();
+     } else {
+          pagetop.fadeOut();
+     }
+  });
+  pagetop.click(function () {
+     $('body, html').animate({ scrollTop: 0 }, 500);
+     return false;
+  });
 });
